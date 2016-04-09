a1b47953b71b941be8969e73cc7a965b29610b69
Afroze Khan <auk2@njit.edu>
2016-04-08 21:22:26 -0400

    added openshift config changes

--- server.js
+++ server.js
@@ -63,8 +63,15 @@ app.get('/',function(req,res){
 //===============================
 
 //app.listen(config.port);
-app.listen(port)
-console.log('Something bout to go down on port: ' + port);
+// app.listen(port)
+// console.log('Something bout to go down on port: ' + port);
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
 
 
 
