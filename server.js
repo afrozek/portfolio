1050b0ee150ceb0fc846866a479d6e466ac6450c
froost <froost@froosts-MBP.home>
2017-11-26 19:25:57 -0500

    port 80

--- server.js
+++ server.js
@@ -66,11 +66,11 @@ app.get('/',function(req,res){
 // app.listen(port)
 // console.log('Something bout to go down on port: ' + port);
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || port;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || port;
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
 app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+  console.log( "Listening on port:" + port )
 });
 
 
